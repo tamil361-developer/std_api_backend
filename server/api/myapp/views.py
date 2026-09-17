4de7dd11688e7118b024ceb5fb611d6252d00d92
tamil361-developer <tamileniyan361@gmail.com>
2026-09-17 10:36:59 +0530

    Paginate order details endpoint

--- server/api/myapp/views.py
+++ server/api/myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
 from .models import customers as CustomerModel, orders as OrderModel, order_details as OrderDetailsModel, products as ProductModel
 from .serializer import CustomerSerializer,OrderSerializer,OrderDetailsSerializer,ProductSerializer 
@@ -29,9 +30,13 @@ def orders(request):
 @api_view(['GET'])
 def order_details(request):
     if request.method == 'GET':
-        order_detail = OrderDetailsModel.objects.all()
-        serializer = OrderDetailsSerializer(order_detail, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        order_details = OrderDetailsModel.objects.all().order_by('id')
+        paginator = PageNumberPagination()
+        paginator.page_size = 100
+        paginator.max_page_size = 1000
+        page = paginator.paginate_queryset(order_details, request)
+        serializer = OrderDetailsSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
 @api_view(['GET'])
 def products(request):
