b16f196c5697221f7d963b5aeb9062d2f2ca425c
tamil361-developer <tamileniyan361@gmail.com>
2026-09-17 10:27:45 +0530

    Support hyphenated order details endpoint

--- server/api/myapp/urls.py
+++ server/api/myapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('customers/', views.customers, name='customers'),
     path('orders/', views.orders, name='orders'),
     path('order_details/', views.order_details, name='order-details'),
+    path('order-details/', views.order_details, name='order-details-hyphenated'),
     path('products/', views.products, name='products'),
 ]
